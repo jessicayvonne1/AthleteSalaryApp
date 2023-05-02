b60cc4ee0198eb9bcde281de5673ecd9e6fdc394
jessicayvonne1 <57275202+jessicayvonne1@users.noreply.github.com>
2023-05-02 12:25:36 -0400

    Add files via upload

---
+++ AthleteSalaryApp/src/athletesalaryapp/AthleteSalaryApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package athletesalaryapp;
+
+/**
+ *
+ * @author Jess
+ */
+public class AthleteSalaryApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AthleteSalaryGUI a = new AthleteSalaryGUI();
+        a.setVisible(true);
+        
+    }
+    
+}
